15cc19da8a26e7980b89e55b96799b233f8d7b43
guiioshua <guilhermeioshua@gmail.com>
2026-04-04 18:30:31 -0300

    feat: modelos dos dias restritos e sabados letivos. Ambos com referencia para um semestre, a ser implementado

---
+++ sigafeliz/src/main/java/com/sigafeliz/model/DiaRestrito.java
@@ -0,0 +1,9 @@
+package com.sigafeliz.model;
+
+import java.time.LocalDate;
+
+public class DiaRestrito {
+    private Semestre semestre;
+    private LocalDate data;
+    private String descricao;
+}
---
+++ sigafeliz/src/main/java/com/sigafeliz/model/SabadoLetivo.java
@@ -0,0 +1,8 @@
+package com.sigafeliz.model;
+
+import java.time.LocalDate;
+
+public class SabadoLetivo {
+    private Semestre semestre;
+    private LocalDate data;
+}
