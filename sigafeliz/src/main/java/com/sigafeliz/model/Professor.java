a3b459e91a91a9626b1009d8ac4d038a3481111c
guiioshua <guilhermeioshua@gmail.com>
2026-04-04 18:29:48 -0300

    feat: classe de professor, encapsulamento básico

---
+++ sigafeliz/src/main/java/com/sigafeliz/model/Professor.java
@@ -0,0 +1,19 @@
+package com.sigafeliz.model;
+
+// Professor existe de forma independente das disciplinas. Não é preciso ter lista de Disciplinas aqui.
+public class Professor {
+
+    private String nome;
+    private String email;
+
+    public Professor(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getEmail() {return email; }
+    public void setEmail(String email) { this.email = email;}
+}
