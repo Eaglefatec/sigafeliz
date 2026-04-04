79dce325b4fc719808a372b86bb279a13315c63f
guiioshua <guilhermeioshua@gmail.com>
2026-04-04 19:18:27 -0300

    feat: getters e setters dos dias restriso e sabados letivos

--- sigafeliz/src/main/java/com/sigafeliz/model/DiaRestrito.java
+++ sigafeliz/src/main/java/com/sigafeliz/model/DiaRestrito.java
@@ -6,4 +6,19 @@ public class DiaRestrito {
     private Semestre semestre;
     private LocalDate data;
     private String descricao;
+
+    public DiaRestrito(Semestre semestre, LocalDate data, String descricao) {
+        this.semestre = semestre;
+        this.data = data;
+        this.descricao = descricao;
+    }
+
+    public Semestre getSemestre() { return semestre; }
+    public void setSemestre(Semestre semestre) { this.semestre = semestre; }
+
+    public LocalDate getData() { return data; }
+    public void setData(LocalDate data) { this.data = data; }
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
 }
--- sigafeliz/src/main/java/com/sigafeliz/model/SabadoLetivo.java
+++ sigafeliz/src/main/java/com/sigafeliz/model/SabadoLetivo.java
@@ -5,4 +5,11 @@ import java.time.LocalDate;
 public class SabadoLetivo {
     private Semestre semestre;
     private LocalDate data;
+
+    public Semestre getSemestre() { return semestre; }
+    public void setSemestre(Semestre semestre) { this.semestre = semestre; }
+
+    public LocalDate getData() { return data; }
+    public void setData(LocalDate data) { this.data = data; }
+
 }
