8f532a1445e7433b4f063acb24244e3172b7c131
guiioshua <guilhermeioshua@gmail.com>
2026-04-04 17:21:52 -0300

    chore: condições e configurações de maven para build do projeto

---
+++ sigafeliz/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.sigafeliz.sigafeliz {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.sigafeliz to javafx.fxml;
+    exports com.sigafeliz;
+    exports com.sigafeliz.controller;
+    opens com.sigafeliz.controller to javafx.fxml;
+}
