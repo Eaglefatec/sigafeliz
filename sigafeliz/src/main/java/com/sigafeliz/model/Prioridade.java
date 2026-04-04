1f318ed180e4764ede27782bf90981b69162e5e1
guiioshua <guilhermeioshua@gmail.com>
2026-04-04 17:31:30 -0300

    feat: enum de opções de prioridades

---
+++ sigafeliz/src/main/java/com/sigafeliz/model/Prioridade.java
@@ -0,0 +1,9 @@
+package com.sigafeliz.model;
+
+// Enums são classes que definem um conjunto de constantes que podem ser usadas como dados.
+// Nesse caso, são usadas porque há uma coluna de uma tabela com um conjunto de opções selecionáveis e fixas.
+public enum Prioridade {
+    ALTA,
+    MEDIA,
+    BAIXA;
+}
