30e7fb9718929965a795f3cb12b74d8230553b4d
guiioshua <guilhermeioshua@gmail.com>
2026-04-05 18:07:36 -0300

    feat: adição de construtor de sabado letivo

--- sigafeliz/src/main/java/com/sigafeliz/model/SabadoLetivo.java
+++ sigafeliz/src/main/java/com/sigafeliz/model/SabadoLetivo.java
@@ -6,10 +6,14 @@ public class SabadoLetivo {
     private Semestre semestre;
     private LocalDate data;
 
+    public SabadoLetivo(Semestre semestre, LocalDate data) {
+        this.semestre = semestre;
+        this.data = data;
+    }
+
     public Semestre getSemestre() { return semestre; }
     public void setSemestre(Semestre semestre) { this.semestre = semestre; }
 
     public LocalDate getData() { return data; }
     public void setData(LocalDate data) { this.data = data; }
-
 }
