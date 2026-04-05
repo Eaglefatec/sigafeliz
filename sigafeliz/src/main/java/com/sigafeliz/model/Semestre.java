5cb4c6b2aa7112a3820cc854e1f57d192e0cfeab
guiioshua <guilhermeioshua@gmail.com>
2026-04-05 18:06:53 -0300

    refac: reorganziação do código do semestre

--- sigafeliz/src/main/java/com/sigafeliz/model/Semestre.java
+++ sigafeliz/src/main/java/com/sigafeliz/model/Semestre.java
@@ -1,30 +1,36 @@
 package com.sigafeliz.model;
 
-import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Semestre {
+    
     private String nome;
     private LocalDate dataInicio;
     private LocalDate dataFim;
     private LocalDate dataKickoff;
 
-    // A existência dos dias restritos e sabados letivos está fortemente acoplada ao semestre a qual pertencem.
-    // Nesse caso, definimos uma referência à lista dentro da classe para ambos serem persistidos juntos.
-    // final é usado para que a referencia (variável) das listas sejam sempre constantes.
-    private final List<SabadoLetivo> sabadosLetivos;
-    private final List<DiaRestrito> diasRestritos;
+    private List<SabadoLetivo> sabadosLetivos;
+    private List<DiaRestrito> diasRestritos;
 
     public Semestre(String nome, LocalDate dataInicio, LocalDate dataFim, LocalDate dataKickoff) {
         this.nome = nome;
         this.dataInicio = dataInicio;
         this.dataFim = dataFim;
         this.dataKickoff = dataKickoff;
-        sabadosLetivos = new ArrayList<>();
-        diasRestritos = new ArrayList<>();
+        this.sabadosLetivos = new ArrayList<>();
+        this.diasRestritos = new ArrayList<>();
+    }
+
+    public void addSabadoLetivo(SabadoLetivo sabado) {
+        this.sabadosLetivos.add(sabado);
+        sabado.setSemestre(this);
+    }
+
+    public void addDiaRestrito(DiaRestrito dia) {
+        this.diasRestritos.add(dia);
+        dia.setSemestre(this);
     }
 
     public String getNome() { return nome; }
@@ -39,15 +45,6 @@ public class Semestre {
     public LocalDate getDataKickoff() { return dataKickoff; }
     public void setDataKickoff(LocalDate dataKickoff) { this.dataKickoff = dataKickoff; }
 
-    // Ao invés de setters normais, somente queremos adicionar ou remover itens da lista.
-    // Um setter definiria uma nova lista toda vez que chamassemos.
     public List<SabadoLetivo> getSabadosLetivos() { return sabadosLetivos; }
-    public void addSabadoLetivo(SabadoLetivo sabadoLetivo) {
-        this.sabadosLetivos.add(sabadoLetivo);
-    }
-
     public List<DiaRestrito> getDiasRestritos() { return diasRestritos; }
-    public void addDiaRestrito(DiaRestrito diaRestrito) {
-        this.diasRestritos.add(diaRestrito);
-    }
 }
