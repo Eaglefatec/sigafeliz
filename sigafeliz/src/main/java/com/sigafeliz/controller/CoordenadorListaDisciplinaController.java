09ea33e241d58cc84dcde5158e7a8ecdde8b3910
Fernando <fercaprh@gmail.com>
2026-04-29 22:48:13 -0300

    fix(CoordenadorListadisciplinaController): add campo comboBox de 40/80 horas no INSERT

--- sigafeliz/src/main/java/com/sigafeliz/controller/CoordenadorListaDisciplinaController.java
+++ sigafeliz/src/main/java/com/sigafeliz/controller/CoordenadorListaDisciplinaController.java
@@ -1,11 +1,15 @@
 package com.sigafeliz.controller;
 
 import com.sigafeliz.Main;
+import com.sigafeliz.model.Professor;
+import com.sigafeliz.service.ProfessorService;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.util.StringConverter;
 
 public class CoordenadorListaDisciplinaController {
 
@@ -22,10 +26,42 @@ public class CoordenadorListaDisciplinaController {
     private Button btnVoltar;
 
     @FXML
-    private ComboBox<?> cbProfessor;
+    private ComboBox<Integer> comboCargaHoraria;
+
+    @FXML
+    private ComboBox<Professor> comboProfessores;
 
     @FXML
     private TextField txtDisciplina;
+
+    // MÉTODO INICIAL (carrega dados)
+    @FXML
+    public void initialize() {
+        carregarProfessores();
+        carregarCargaHoraria();
+    }
+
+    private void carregarProfessores() {
+        // Carrega a lista de professores do mongo para o ComboBox
+        comboProfessores.setItems(FXCollections.observableArrayList(ProfessorService.getAllProfessores()));
+        // Configura como o Professor será exibido no ComboBox (apenas o nome)
+        comboProfessores.setConverter(new StringConverter<Professor>() {
+            @Override
+            public String toString(Professor p) {
+                return p == null ? "" : p.getNome();
+            }
+            @Override
+            public Professor fromString(String string) {
+                return null;
+            }
+        });
+
+    }
+
+    private void carregarCargaHoraria() {
+        comboCargaHoraria.getItems().clear();
+        comboCargaHoraria.getItems().addAll(40, 80);
+    }
 
     @FXML
     void abrirEditarGrade(ActionEvent event) {
