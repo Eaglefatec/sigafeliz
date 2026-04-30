d8f11397593b3063b9d605878350ed6801d3094a
Rafael-SantosR <rafael.spsjcpt@gmail.com>
2026-04-30 15:53:49 -0300

    refactor(GradeDAO): atualiza do GradeDAO

--- sigafeliz/src/main/java/com/sigafeliz/dao/GradeDAO.java
+++ sigafeliz/src/main/java/com/sigafeliz/dao/GradeDAO.java
@@ -1,6 +1,30 @@
 package com.sigafeliz.dao;
 
+import com.sigafeliz.model.AulasPorDia;
+import com.sigafeliz.infra.ConexaoDB;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class GradeDAO{
+
+    public void salvarGrade(AulasPorDia a) throws SQLException {
+        String sql = """
+                INSERT INTO aula_por_dia (disciplina_nome, dia_semana, quantidade_aulas)
+                VALUES (?, ?, ?)
+                """;
+
+        try (Connection con = ConexaoDB.getConexao();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setString(1, a.getDisciplina().getNome());
+            ps.setString(2, a.getDiaSemana().name());
+            ps.setInt(3, a.getQuantidadeAulas());
+
+            ps.executeUpdate();
+        }
+
+    }
 
 
 }
